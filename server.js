3136298743953ec3399f0c68ba97e2b402c9f957
mutharasus <mutharasu.s@gmail.com>
2021-08-11 05:09:15 +0000

    Switching to replit secrets

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
+//require('dotenv').config();//uncomment if not using replit secrets
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,7 +10,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+//const uri = process.env.ATLAS_URI;//uncomment if not using replit secrets and store secret in .env file in root directory
+
+const uri = process.env['ATLAS_URI'];
+
+
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
 connection.once('open', () => {
